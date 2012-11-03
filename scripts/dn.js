f6eae29c889ba586f90dff63097e60632d4f9a18
redaxmedia <info@redaxmedia.com>
2012-11-03 01:41:46 +0100

    Even more workover on CSS and JS #4

--- scripts/dn.js
+++ scripts/dn.js
@@ -427,7 +427,7 @@
 					}
 					else
 					{
-						dn.score - 2;
+						dn.score--;
 						output += 'fail';
 					}
 					output += '" title="Ninja: ' + dn.setup[i].amountNinja + ' | Trainee: ' + dn.setup[i].amountTrainee + ' | Novice: ' + dn.setup[i].amountNovice + '">' + dn.setup[i].description + ': ' + dn.setup[i].amount + '</li>';
