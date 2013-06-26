f0d153cb4d22abe2de04ec9ef24f70501e50bd5f
redaxmedia <info@redaxmedia.com>
2013-06-26 21:26:16 +0200

    Update composer.json and gruntfile.js

--- gruntfile.js
+++ gruntfile.js
@@ -80,7 +80,7 @@ module.exports = function (grunt)
 
 	/* register tasks */
 
-	grunt.registerTask('default', ['jshint']);
+	grunt.registerTask('default', ['jshint', 'csslint']);
 	grunt.registerTask('toc', ['shell:toc']);
 	grunt.registerTask('deploy', ['uglify', 'cssmin']);
 };
